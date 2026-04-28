d937d95b9d771ba7036af0dd95db661166a43e32
Phyu Han <phyuhanthwe948@gmail.com>
2026-04-28 23:53:33 +0700

    simple-deployment-pipeline

---
+++ playwright.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require('@playwright/test')
+
+module.exports = defineConfig({
+  testDir: './e2e-tests',
+  use: {
+    baseURL: 'http://localhost:5001',
+    headless: true,
+  },
+  webServer: {
+    command: 'npm run start:test',
+    port: 5001,
+    reuseExistingServer: !process.env.CI,
+  },
+})
